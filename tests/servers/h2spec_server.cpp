37cdb1034a8c41c7a8128efc2cd311bc5785a65b
kelbon <you@example.com>
2025-09-16 11:41:22 +0500

    refactor h2spec test

--- tests/servers/h2spec_server.cpp
+++ tests/servers/h2spec_server.cpp
@@ -1,17 +1,11 @@
 #include "h2spec_server.hpp"
-#include <csignal>
 #include <iostream>
 
-http2::h2spec_server server;
 int main() try {
-  std::signal(
-      SIGINT, +[](int) {
-        std::cout << "interrupted!";
-        server.ioctx().stop();
-      });
   asio::ip::tcp::endpoint ipv4_endpoint(asio::ip::address_v4::loopback(), 2999);
+  http2::h2spec_server server;
   server.listen({ipv4_endpoint});
-
+  std::cout << std::flush;
   server.ioctx().run();
 } catch (std::exception& e) {
   std::cout << e.what() << std::endl;
--- tests/test_h2spec.cpp
+++ tests/test_h2spec.cpp
@@ -1,43 +1,104 @@
 
 #include <iostream>
 #include <string>
+#include <thread>
 
-#include <boost/process.hpp>
+#include <boost/asio.hpp>
+#include <boost/process/v2.hpp>
+#include <boost/process/v2/environment.hpp>
+
+#include <kelcoro/task.hpp>
+#include <http2/asio/awaiters.hpp>
 
 namespace bp = boost::process;
 
-int main() {
+static dd::task<void> read_until_mark(asio::readable_pipe& pipe, std::string& output, std::string mark = {}) {
+  using http2::io_error_code;
+  using http2::net;
+  http2::byte_t buf[128];
+  io_error_code ec;
+  for (;;) {
+    size_t sz = co_await net.read_some(pipe, buf, ec);
+    if (ec) {
+      std::cout << ec.what() << std::endl;
+      break;
+    }
+    output += std::string_view((const char*)buf, sz);
+    if (!mark.empty() && output.find(mark) != output.npos)
+      break;
+  }
+}
+
+int main() try {
   std::thread([] {
     std::this_thread::sleep_for(std::chrono::seconds(20));
     std::cout << "TEST TIMEOUT" << std::endl;
     std::exit(-1);
   }).detach();
-  try {
-    std::future<std::string> foutput;
 
-    bp::ipstream pipe_stream;
-    auto cwd = boost::filesystem::current_path();
+  asio::io_context ctx;
+
+  asio::readable_pipe server_pipe(ctx);
+
 #ifdef _WIN32
-    bp::child server(cwd / "servers/h2spec_server.exe");
+  auto serverpath = "servers/h2spec_server.exe";
 #else
-    bp::child server(cwd / "servers/h2spec_server");
+  auto serverpath = "servers/h2spec_server";
 #endif
-    std::this_thread::sleep_for(std::chrono::seconds(1));  // time for server starting
-    (void)bp::system("h2spec --port 2999", bp::std_out > foutput, bp::std_err > bp::null);
-    std::string output = foutput.get();
-    if (output.find("146 tests, 145 passed, 0 skipped, 1 failed") == output.npos) {
-      std::cout << "146 tests NOT FOUND, output: " << output;
-      return EXIT_FAILURE;
-    }
-    if (output.find("4.2. Maximum Table Size") == output.npos) {
-      std::cout << "4.2. Maximum Table Size NOT FOUND, output: " << output;
-      return EXIT_FAILURE;
-    }
-    server.terminate();
-    std::cout << "SUCCESS" << std::endl;
-    return EXIT_SUCCESS;
-  } catch (const bp::process_error& e) {
-    std::cerr << "BOOST PROCESS ERROR: " << e.what() << std::endl;
-    return 1;
+  bp::process server(ctx, serverpath, {}, bp::process_stdio{.out = server_pipe});
+
+  asio::readable_pipe h2spec_pipe(ctx);
+  bp::process h2spec(ctx, bp::environment::find_executable("h2spec"), {"--port", "2999", "-o", "1"},
+                     bp::process_stdio{.out = h2spec_pipe});
+  std::string h2specstr;
+  std::string serverstr;
+  // h2spec_server flushes output after this log to avoid deadlock
+  auto h =
+      read_until_mark(server_pipe, serverstr, "Server listening on").start_and_detach(/*stop_at_end=*/true);
+  on_scope_exit {
+    h.destroy();
+  };
+  while (!h.done())
+    ctx.run_one();
+
+  // read server output to avoid deadlock on std::cout, when pipe if full
+  auto h2 = read_until_mark(server_pipe, serverstr).start_and_detach(true);
+  on_scope_exit {
+    h2.destroy();
+  };
+  auto h3 = read_until_mark(h2spec_pipe, h2specstr).start_and_detach(true);
+  on_scope_exit {
+    h3.destroy();
+  };
+  while (h2spec.running())
+    ctx.poll();
+  try {
+    h2spec.wait();
+  } catch (std::exception& e) {
+    std::cout << "wait failed with " << e.what() << std::endl;
   }
+  server_pipe.cancel();
+  h2spec_pipe.cancel();
+  server_pipe.close();
+  server.terminate();
+
+  // h2spec end running, but only here writes last chunks of output
+  ctx.poll();
+  if (auto i = h2spec.exit_code(); i != 1) {
+    std::cout << "invalid error code, expected 1, actual: " << i << std::endl;
+    return EXIT_FAILURE;
+  }
+  if (h2specstr.find("146 tests, 145 passed, 0 skipped, 1 failed") == h2specstr.npos) {
+    std::cout << "146 tests NOT FOUND, output: " << h2specstr << std::endl;
+    return EXIT_FAILURE;
+  }
+  if (h2specstr.find("4.2. Maximum Table Size") == h2specstr.npos) {
+    std::cout << "4.2. Maximum Table Size NOT FOUND, output: " << h2specstr << std::endl;
+    return EXIT_FAILURE;
+  }
+  std::cout << "SUCCESS" << std::endl;
+  return EXIT_SUCCESS;
+} catch (const std::exception& e) {
+  std::cerr << "BOOST PROCESS ERROR: " << e.what() << std::endl;
+  return 1;
 }
